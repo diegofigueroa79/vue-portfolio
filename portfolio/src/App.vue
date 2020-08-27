0c23fecf093118f7f59e9fe3446ca638a8329e0a
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-08-26 22:19:38 -0700

    integrated Navigation comp

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <v-app>
+    <Navigation />
     
   </v-app>
 </template>
 
 <script>
 
+import Navigation from './components/home/Navigation'
+
 export default {
   name: 'App',
 
   components: {
+    Navigation
   },
 
   data: () => ({
