715895588024f67968526e02dc5be9ab2db725f0
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-08-28 00:59:55 -0700

    created title row and subtitle row

--- portfolio/src/components/diary/Diary.vue
+++ portfolio/src/components/diary/Diary.vue
@@ -1,5 +1,24 @@
-<template>
-  <h1>Diary</h1>
+ <template>
+    <v-col class="red mt-12" offset=2 cols=8>
+        <!-- title row -->
+        <v-row justify="center">
+            <v-col class="blue" cols=4>
+                <div class="display-1 text-center">My Diary</div>
+            </v-col>
+        </v-row>
+        <!-- subtitle row -->
+        <v-row>
+            <v-col class="green" cols=6>
+                <div class="ml-4"><h2>Entries</h2></div>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col class="yellow" cols=2>
+                <v-btn>Add <v-icon right>mdi-plus</v-icon> </v-btn>
+            </v-col>
+        </v-row>
+        <!-- entries row -->
+
+    </v-col>
 </template>
 
 <script>
