3d759166a2b0554865a306f4925ea546aab402e6
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-08-30 18:05:07 -0700

    created modules folder with diary module

--- portfolio/src/store/index.js
+++ portfolio/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import diary from './modules/diary'
 
 Vue.use(Vuex)
 
@@ -15,6 +16,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    diary
   },
   getters: {
     getDrawer: state => state.drawer
