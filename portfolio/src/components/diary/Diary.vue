f34f25294d92e34e2796902d80cd5abb5ed3996e
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-09-02 02:57:51 -0700

    changed several styles, colors, and added divider

--- portfolio/src/components/diary/Diary.vue
+++ portfolio/src/components/diary/Diary.vue
@@ -1,18 +1,23 @@
  <template>
-    <v-col class="red mt-12" offset=2 cols=8>
+    <v-col 
+        class="purple lighten-4 mt-12 rounded-xl elevation-10" 
+        offset=2 
+        cols=8>
         <!-- title row -->
         <v-row justify="center">
-            <v-col class="blue" cols=4>
+            <v-col class="purple--text" cols=4>
                 <div class="display-1 text-center">My Diary</div>
             </v-col>
         </v-row>
         <!-- subtitle row -->
         <v-row>
-            <v-col class="green" cols=6>
-                <div class="ml-4"><h2>Entries</h2></div>
+            <v-col cols=6>
+                <div class="purple--text text--lighten-1 ml-4">
+                    <h2>My Entries</h2>
+                </div>
             </v-col>
             <v-spacer></v-spacer>
-            <v-col class="yellow" cols=2>
+            <v-col cols=2>
                 <Entry>Add <v-icon right>mdi-plus</v-icon></Entry>
             </v-col>
         </v-row>
@@ -20,21 +25,27 @@
         <v-row>
             <v-col>
                 <v-card>
-                    <v-list>
-                        <v-list-item
-                            v-for="(entry, i) in getEntries"
-                            :key="i"
+                    <v-list class="purple lighten-5">
+                        <template
+                            v-for="(entry, i) in getEntries"  
                         >
-                            <v-list-item-content>
-                                <Entry v-bind:active="true" v-bind:entry="entry">
-                                    <v-list-item-title class="text-left" v-text="entry.title">
-                                    </v-list-item-title>
-                                </Entry>
-                            </v-list-item-content>
-                            <v-list-item-icon>
-                                <v-btn v-on:click.stop="deleteEntry(i)" icon><v-icon>mdi-delete</v-icon></v-btn>
-                            </v-list-item-icon>
-                        </v-list-item>
+                            <v-divider :key="i+1" v-if="i > 0"></v-divider>
+                            <v-list-item
+                                :key="i"
+                            >
+                                <v-list-item-content>
+                                    <Entry v-bind:active="true" v-bind:entry="entry">
+                                        <v-list-item-title
+                                            class="text-left purple--text" 
+                                            v-text="entry.title">
+                                        </v-list-item-title>
+                                    </Entry>
+                                </v-list-item-content>
+                                <v-list-item-icon>
+                                    <v-btn v-on:click.stop="deleteEntry(i)" icon><v-icon>mdi-delete</v-icon></v-btn>
+                                </v-list-item-icon>
+                            </v-list-item>
+                        </template>
                     </v-list>
                 </v-card>
             </v-col>
