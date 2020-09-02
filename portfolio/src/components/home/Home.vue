1f377b9d5a7dafce65d5c0dc3159ad0855a68d3c
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-09-02 16:48:15 -0700

    added Vue and vuetify logos, header, and column layout

--- portfolio/src/components/home/Home.vue
+++ portfolio/src/components/home/Home.vue
@@ -1,12 +1,54 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
-  </div>
+  <v-col
+    class="purple lighten-4"
+    cols=12
+  >
+    <v-row>
+      <v-col
+        class="purple d-flex justify-center"
+        cols=3
+        offset=3
+      >
+        <v-img
+          height="200"
+          width="200"
+          src="../../assets/logo.svg"
+        ></v-img>
+      </v-col>
+      <v-col
+        class="blue d-flex justify-center"
+        cols=3
+      >
+        <v-img
+          height="220"
+          width="220"
+          src="../../assets/logo.png"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h1 class="text-center blue--text">Vue CLI, Vuetify, Vuex, Vue Router</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <template v-for="(n, index) in 3">
+        <v-col :key="index" class="white rounded-xl d-flex justify-center">
+          Hello
+        </v-col>
+      </template>
+    </v-row>
+  </v-col>
 </template>
 
 <script>
 export default {
-    name: 'Home'
+    name: 'Home',
+    data: () => ({
+      projects: [
+        { title: '', description: '', link: ''}
+      ]
+    })
 }
 </script>
 
