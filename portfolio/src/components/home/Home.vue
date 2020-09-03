46a33bc584aeb17d90e1c6d9d5be1d2b06c89e2c
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-09-03 10:23:08 -0700

    fixed aspect ratios of logos, removed column colors

--- portfolio/src/components/home/Home.vue
+++ portfolio/src/components/home/Home.vue
@@ -1,25 +1,26 @@
 <template>
   <v-col
-    class="purple lighten-4"
     cols=12
   >
     <v-row>
       <v-col
-        class="purple d-flex justify-center"
+        class="d-flex justify-center"
         cols=3
         offset=3
       >
         <v-img
+          contain
           height="200"
           width="200"
           src="../../assets/logo.svg"
         ></v-img>
       </v-col>
       <v-col
-        class="blue d-flex justify-center"
+        class="d-flex justify-center"
         cols=3
       >
         <v-img
+          contain
           height="220"
           width="220"
           src="../../assets/logo.png"
@@ -33,7 +34,7 @@
     </v-row>
     <v-row>
       <template v-for="(project, index) in projects">
-        <v-col :key="index" class="white rounded-xl d-flex justify-center">
+        <v-col :key="index" class="d-flex justify-center">
           <v-hover v-slot:default="{ hover }">
             <v-card
               class="mx-auto"
