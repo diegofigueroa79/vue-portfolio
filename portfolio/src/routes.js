57e1764cdcbd0698273038eea15dbc187663491d
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-09-14 12:34:50 -0700

    routed Calculator compo

--- portfolio/src/routes.js
+++ portfolio/src/routes.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 
 import Home from './components/home/Home'
 import Diary from './components/diary/Diary'
+import Calculator from './components/calculator/Calculator'
 
 Vue.use(VueRouter)
 
@@ -11,6 +12,7 @@ export const router = new VueRouter({
     mode: 'history',
     routes: [
         { path: '/', component: Home },
-        { path: '/diary/', component: Diary }
+        { path: '/diary/', component: Diary },
+        { path: '/calculator/', component: Calculator }
     ]
 })
