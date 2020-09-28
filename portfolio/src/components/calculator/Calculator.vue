b41facb99b64cd64256e90e22e2b231ed8a54e43
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-09-27 17:25:20 -0700

    rounded shape of calculator and added appropriate colors

--- portfolio/src/components/calculator/Calculator.vue
+++ portfolio/src/components/calculator/Calculator.vue
@@ -1,40 +1,40 @@
 <template> 
-    <v-col class="red">
+    <v-col>
         <v-row justify="center">
-            <v-col class="blue" cols=auto>
+            <v-col class="blue-grey rounded" cols=auto>
                 <v-row>
-                    <v-col class="yellow mx-3 display-1">
+                    <v-col class="amber blue-grey--text rounded mx-3 display-1">
                         {{current}}
                     </v-col>
                 </v-row>
                 <v-row>
-                    <v-col class="yellow ml-3 pr-1 mr-0 mt-2" @click="clear"><v-btn large>C</v-btn></v-col>
-                    <v-col class="red mx-0 px-1 mt-2" @click="sign"><v-btn large>+/-</v-btn></v-col>
-                    <v-col class="green mx-0 px-1 mt-2"><v-btn large>%</v-btn></v-col>
-                    <v-col class="white ml-0 pl-1 mr-3 mt-2" @click="divide"><v-btn large>/</v-btn></v-col>
+                    <v-col class="ml-3 pr-1 mr-0 mt-2" @click="clear"><v-btn class="grey lighten-2 font-weight-black" large>C</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-2" @click="sign"><v-btn class="grey lighten-2 font-weight-black" large>+/-</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-2"><v-btn class="grey lighten-2 font-weight-black" large>%</v-btn></v-col>
+                    <v-col class="ml-0 pl-1 mr-3 mt-2" @click="divide"><v-btn class="grey lighten-2 font-weight-black" large>/</v-btn></v-col>
                 </v-row>
                 <v-row>
-                    <v-col class="yellow ml-3 pr-1 mr-0 mt-0" @click="aggregate('7')"><v-btn large>7</v-btn></v-col>
-                    <v-col class="red mx-0 px-1 mt-0" @click="aggregate('8')"><v-btn large>8</v-btn></v-col>
-                    <v-col class="green mx-0 px-1 mt-0" @click="aggregate('9')"><v-btn large>9</v-btn></v-col>
-                    <v-col class="white ml-0 pl-1 mr-3 mt-0" @click="multiply"><v-btn large>X</v-btn></v-col>
+                    <v-col class="ml-3 pr-1 mr-0 mt-0 pt-0" @click="aggregate('7')"><v-btn class="grey lighten-2 font-weight-black" large>7</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-0 pt-0" @click="aggregate('8')"><v-btn class="grey lighten-2 font-weight-black" large>8</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-0 pt-0" @click="aggregate('9')"><v-btn class="grey lighten-2 font-weight-black" large>9</v-btn></v-col>
+                    <v-col class="ml-0 pl-1 mr-3 mt-0 pt-0" @click="multiply"><v-btn class="grey lighten-2 font-weight-black" large>X</v-btn></v-col>
                 </v-row>
                 <v-row>
-                    <v-col class="yellow ml-3 pr-1 mr-0 mt-0" @click="aggregate('4')"><v-btn large>4</v-btn></v-col>
-                    <v-col class="red mx-0 px-1 mt-0" @click="aggregate('5')"><v-btn large>5</v-btn></v-col>
-                    <v-col class="green mx-0 px-1 mt-0" @click="aggregate('6')"><v-btn large>6</v-btn></v-col>
-                    <v-col class="white ml-0 pl-1 mr-3 mt-0" @click="subtract"><v-btn large>-</v-btn></v-col>
+                    <v-col class="ml-3 pr-1 mr-0 mt-0 pt-0" @click="aggregate('4')"><v-btn class="grey lighten-2 font-weight-black" large>4</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-0 pt-0" @click="aggregate('5')"><v-btn class="grey lighten-2 font-weight-black" large>5</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-0 pt-0" @click="aggregate('6')"><v-btn class="grey lighten-2 font-weight-black" large>6</v-btn></v-col>
+                    <v-col class="ml-0 pl-1 mr-3 mt-0 pt-0" @click="subtract"><v-btn class="grey lighten-2 font-weight-black" large>-</v-btn></v-col>
                 </v-row>
                 <v-row>
-                    <v-col class="yellow ml-3 pr-1 mr-0 mt-0" @click="aggregate('1')"><v-btn large>1</v-btn></v-col>
-                    <v-col class="red mx-0 px-1 mt-0" @click="aggregate('2')"><v-btn large>2</v-btn></v-col>
-                    <v-col class="green mx-0 px-1 mt-0" @click="aggregate('3')"><v-btn large>3</v-btn></v-col>
-                    <v-col class="white ml-0 pl-1 mr-3 mt-0" @click="add"><v-btn large>+</v-btn></v-col>
+                    <v-col class="ml-3 pr-1 mr-0 mt-0 pt-0" @click="aggregate('1')"><v-btn class="grey lighten-2 font-weight-black" large>1</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-0 pt-0" @click="aggregate('2')"><v-btn class="grey lighten-2 font-weight-black" large>2</v-btn></v-col>
+                    <v-col class="mx-0 px-1 mt-0 pt-0" @click="aggregate('3')"><v-btn class="grey lighten-2 font-weight-black" large>3</v-btn></v-col>
+                    <v-col class="ml-0 pl-1 mr-3 mt-0 pt-0" @click="add"><v-btn class="grey lighten-2 font-weight-black" large>+</v-btn></v-col>
                 </v-row>
                 <v-row>
-                    <v-col class="yellow ml-3 pr-1 mr-0 mt-0" @click="aggregate('0')"><v-btn large>0</v-btn></v-col>
-                    <v-col class="green mx-1 mt-0" @click="dot"><v-btn large>.</v-btn></v-col>
-                    <v-col class="white ml-0 pl-1 mr-3 mt-0" @click="equals"><v-btn large>=</v-btn></v-col>
+                    <v-col class="ml-3 pr-1 mr-0 mt-0 pt-0" @click="aggregate('0')"><v-btn class="grey lighten-2 font-weight-black" large>0</v-btn></v-col>
+                    <v-col class="mx-1 mt-0 pt-0" @click="dot"><v-btn class="grey lighten-2 font-weight-black" large>.</v-btn></v-col>
+                    <v-col class="ml-0 pl-1 mr-3 mt-0 pt-0" @click="equals"><v-btn class="grey lighten-2 font-weight-black" large>=</v-btn></v-col>
                 </v-row>
             </v-col>
         </v-row>
